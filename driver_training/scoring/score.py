ad8524965021903809e6a627bea5e50a0ab7b23d
Thasmika Gokal <tgokal@microsoft.com>
2021-11-22 07:26:38 +0000

    replace score.py and replace test_row data

--- driver_training/scoring/score.py
+++ driver_training/scoring/score.py
@@ -33,58 +33,41 @@ from inference_schema.parameter_types.numpy_parameter_type \
     import NumpyParameterType
 
 
+import json
+import joblib
+import numpy as np
+from azureml.core.model import Model
+
+# Called when the service is loaded
 def init():
-    # load the model from file into a global object
     global model
-
-    # we assume that we have just one model
-    # AZUREML_MODEL_DIR is an environment variable created during deployment.
-    # It is the path to the model folder
-    # (./azureml-models/$MODEL_NAME/$VERSION)
-    model_path = Model.get_model_path(
-        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
-
+    # Get the path to the deployed model file and load it
+    model_path = Model.get_model_path('porto_seguro_safe_driver_model.pkl')
     model = joblib.load(model_path)
 
+# Called when a request is received
+def run(raw_data):
+    # Get the input data as a numpy array
+    data = np.array(json.loads(raw_data)['data'])
+    # Get a prediction from the model
+    predictions = model.predict(data)
+    # Get the corresponding classname for each prediction (0 or 1)
+    classnames = ['no claim', 'claim']
+    predicted_classes = []
+    probabilities = [prediction for prediction in predictions]
+    for probability in probabilities:
+        if probability < 0.5:
+            predicted_classes.append(classnames[0])
+        else:
+            predicted_classes.append(classnames[1])
 
-input_sample = numpy.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
-    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
-output_sample = numpy.array([
-    5021.509689995557,
-    3693.645386402646])
-
-
-# Inference_schema generates a schema for your web service
-# It then creates an OpenAPI (Swagger) specification for the web service
-# at http://<scoring_base_url>/swagger.json
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
-def run(data, request_headers):
-    result = model.predict(data)
-
-    # Demonstrate how we can log custom data into the Application Insights
-    # traces collection.
-    # The 'X-Ms-Request-id' value is generated internally and can be used to
-    # correlate a log entry with the Application Insights requests collection.
-    # The HTTP 'traceparent' header may be set by the caller to implement
-    # distributed tracing (per the W3C Trace Context proposed specification)
-    # and can be used to correlate the request to external systems.
-    print(('{{"RequestId":"{0}", '
-           '"TraceParent":"{1}", '
-           '"NumberOfPredictions":{2}}}'
-           ).format(
-               request_headers.get("X-Ms-Request-Id", ""),
-               request_headers.get("Traceparent", ""),
-               len(result)
-    ))
-
-    return {"result": result.tolist()}
+    # Return the predictions
+    return (probabilities, predicted_classes)
 
 
 if __name__ == "__main__":
     # Test scoring
     init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
+    test_row = '{"data":[[0,1,8,1,0,0,1,0,0,0,0,0,0,0,12,1,0,0,0.5,0.3,0.610327781,7,1,-1,0,-1,1,1,1,2,1,65,1,0.316227766,0.669556409,0.352136337,3.464101615,0.1,0.8,0.6,1,1,6,3,6,2,9,1,1,1,12,0,1,1,0,0,1],[4,2,5,1,0,0,0,0,1,0,0,0,0,0,5,1,0,0,0.9,0.5,0.771362431,4,1,-1,0,0,11,1,1,0,1,103,1,0.316227766,0.60632002,0.358329457,2.828427125,0.4,0.5,0.4,3,3,8,4,10,2,7,2,0,3,10,0,0,1,1,0,1]]}'
     prediction = run(test_row, {})
     print("Test result: ", prediction)
